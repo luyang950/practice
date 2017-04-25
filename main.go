2972268fd45a8802815c7275df398e646bb1c701
luyang <luyang@douyu.tv>
2017-04-25 16:27:08 +0800

    modified main.go

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	var input = []int{3, 5, 2, 1, 6, 8, 7, 9, 4, 10, 19, 18, 16, 15, 13, 14 ,12, 11, 17}
+	mergeSortTest()
+}
+
+func mergeSortTest() {
+	var input = []int{3, 5, 2, 1, 6, 8, 7, 9, 4, 10, 19, 18, 16, 15, 13, 14, 12, 11, 17}
 
 	var res = mysort.MergeSort(input)
 
